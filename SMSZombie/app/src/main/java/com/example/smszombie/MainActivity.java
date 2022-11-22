621911e77c91264d10a840115dbe8f0619141f2d
Manwel Bugeja <manwelbugeja@Manwels-MBP-2.lan>
2022-11-22 11:12:03 +0100

    Added webview that opens when deeplink is clicked

--- SMSZombie/app/src/main/java/com/example/smszombie/MainActivity.java
+++ SMSZombie/app/src/main/java/com/example/smszombie/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.smszombie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.widget.Toast;
@@ -25,5 +26,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent i = new Intent(this, WebViewActivity.class);
+        startActivity(i);
     }
 }
---
+++ SMSZombie/app/src/main/java/com/example/smszombie/WebViewActivity.java
@@ -0,0 +1,59 @@
+package com.example.smszombie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebViewActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+
+        myWebView.getSettings().setLoadsImagesAutomatically(true);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+
+        myWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+                return super.onJsAlert(view, url, message, result);
+            }
+        });
+
+        // opens ALL new links in the webview instead of browser
+        myWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return super.shouldOverrideUrlLoading(view, request);
+            }
+
+            public void onPageFinished(WebView view, String url) {
+            }
+
+        });
+
+        String url = getIntent().getStringExtra("url");
+
+        // Load URL
+        if (url!=null) {
+            myWebView.loadUrl(url);
+        }
+        else {
+            myWebView.loadUrl("https://www.wikipedia.org");
+        }
+    }
+}
