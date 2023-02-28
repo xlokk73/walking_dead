6f76c863577805455ab6c900e44bed3d3f2428c0
Manwel Bugeja <manwelbugeja@Manwels-MBP-2.lan>
2023-02-28 10:11:56 +0100

    fixed mistake

--- APKs/SMS/install.sh
+++ APKs/SMS/install.sh
@@ -2,5 +2,5 @@
 adb root
 adb install ../../SMSZombie/app/build/intermediates/apk/debug/app-debug.apk
 adb shell mkdir /data/user/0/com.example.smszombie/files/
-adb push ../../SMSZombieExtra/app/build/outputs/apk/debug/app-debug.apk /data/user/0/com.example.clipboardzombie/files/dynamic-code.apk
+adb push ../../SMSZombieExtra/app/build/outputs/apk/debug/app-debug.apk /data/user/0/com.example.smszombie/files/dynamic-code.apk
 adb shell mkdir /data/user/0/com.example.smszombie/dumps/ 
